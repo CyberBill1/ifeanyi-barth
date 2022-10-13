9207f1e4c568abbd905c76fc8926d6477095171a
CyberBill1 <104857012+CyberBill1@users.noreply.github.com>
2022-10-14 00:34:00 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+function displayTime(){
+    var dateTime = new Date();
+    var hrs = dateTime.getHours();
+    var min = dateTime.getMinutes();
+    var sec = dateTime.getSeconds();
+    var session = document.getElementById('session');
+
+    if(hrs >= 12){
+        session.innerHTML = 'PM';
+    }else{
+        session.innerHTML = 'AM';
+    }
+
+    if(hrs > 12){
+        hrs = hrs - 12;
+    }
+
+    document.getElementById('hours').innerHTML = hrs;
+    document.getElementById('minutes').innerHTML = min;
+    document.getElementById('seconds').innerHTML = sec;
+}
+setInterval(displayTime, 10);
+console.log("cyber") 
+
+function displayDate(){
+    var date = new Date().toDateString();
+   
+    document.getElementById('date').innerHTML = date;
+}
+setInterval(displayDate, 10);
+console.log("cyber") 
