8b1e45e96131feafba6fb5dc5cb4bf3ba18e99a3
Ede Ifeanyichukwu Barth <104857012+CyberBill1@users.noreply.github.com>
2023-09-27 22:19:33 +0100

    Update front.js

--- js/front.js
+++ js/front.js
@@ -38,6 +38,19 @@ $(function () {
         offset: 80
     });
 
+     // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+
 
     /* =========================================
      * testimonial slider
