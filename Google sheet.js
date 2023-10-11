fa78250d7d3dbb92ada99b5d70c07e526f371bf1
Ede Ifeanyichukwu Barth <104857012+CyberBill1@users.noreply.github.com>
2023-10-11 15:14:36 +0100

    Create Google sheet.js

---
+++ Google sheet.js
@@ -0,0 +1,47 @@
+C:\Users\seemab\Desktop\youtube\codes.New Google Sheet Google Sheet.js Sublime Text (UNREGISTERED) File Edit Selection Find View Goto Tools Project Preferences Help
+
+Google Sheetjs
+
+3 const form document.forms['contact-form']
+
+const ScriptURL
+
+5 form.addEventListener('submit', e => {
+
+Google Sheet.htm
+
+Google Sheets
+
+2
+
+7 fetch(scriptURL, { method: 'POST', body: new FormData(form)}) .then(response => alert("Thank you! your form is submitted successfully."))
+
+.then(() => { window.location.reload(); })
+
+.catch(error => console.error('Error!', error.message))
+
+9
+
+10
+
+11 })
+
+JavaScript
+
+37°C Haze 2:03 PM 6/24/2023
+
+Line 11, Column 3
+
+1
+
+4
+
+e.preventDefault()
+
+6
+
+8
+
+Type here to search
+
+Spaces: 2
