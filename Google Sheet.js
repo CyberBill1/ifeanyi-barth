90cf3af7eae90590bc50f31825ea0ca43a418cd5
Ede Ifeanyichukwu Barth <104857012+CyberBill1@users.noreply.github.com>
2023-10-12 15:28:59 +0100

    Update Google Sheet.js

--- Google Sheet.js
+++ Google Sheet.js
@@ -1,4 +1,4 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbwTqvpIDya8cbE25i4UdXgcGN7XrJq8IirYSmboraqbMF7qipUNIiIp-N395H906k0W/exec'
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwtUbqkhrHz-dgv0UekE-FSr1-dDlZqALq9vJNP343S-tZmm0VBk62Kf-qHWtqotME9/exec'
 
 const form = document.forms['contact-form']
 
