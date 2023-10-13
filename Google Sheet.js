3098d714a66013a21e8f553c19a111573cd47fc8
Ede Ifeanyichukwu Barth <104857012+CyberBill1@users.noreply.github.com>
2023-10-13 15:20:38 +0100

    Update Google Sheet.js

--- Google Sheet.js
+++ Google Sheet.js
@@ -9,8 +9,11 @@ e.preventDefault()
 fetch(scriptURL, { method: 'POST', body: new FormData(form)})
 
 .then(response => alert("Thank you! your form is submitted successfully." ))
+).then((html) => {
+// you can put any JS code here window.open('d.h', '_blank');
+windows.open('www.google.com', '_blank');
 
-.then(() => { window.location.reload(); })
+ // .then(() => { window.location.reload(); })//
 
 .catch(error => console.error('Error!', error.message)) 
 
